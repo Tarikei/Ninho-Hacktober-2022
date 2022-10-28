87c910d5a72d3e2fcdff764171adc63e2880a239
Tarik Ponciano <40394108+TarikPonciano@users.noreply.github.com>
2022-10-28 16:04:48 -0300

    Create JogodoTarik

---
+++ Criações dos Alunos/jogoDoTarik.py
@@ -0,0 +1,19 @@
+print("Bem vindo alunos do Ninho de Desenvolvedores")
+
+print("Nossa jornada começa aqui!")
+
+print("Como toda jornada, ela começa com um primeiro passo...")
+
+print("Bem vindos ao Mundo Gumball")
+
+nome = input("Escolha seu nome: ")
+
+escolha = input("Escolha um dos dois: Casa ou Aula ")
+
+if escolha == "Casa":
+  print("Você foi pra casa")
+
+if escolha == "Aula":
+  print("Você foi pra aula")
+
+print("Fim da Aventura")
