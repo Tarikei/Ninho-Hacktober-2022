3b7dff58327c90126402c7204790604a85f08b68
jeffbcampos <jeffbcampos@gmail.com>
2022-10-29 11:56:09 -0300

    Create helloWorldJs.js
    little function game with hello world

---
+++ Criações dos Alunos/helloWorldJs.js
@@ -0,0 +1,11 @@
+alert("Hello World!");
+
+confirm("Am I Hello World?");
+
+const helloWorld = prompt("What is my name?\nTip: in portuguese they call me = Olá Mundo!");
+
+alert("open the console (press F12 )");
+
+console.log("I'm Hello World 007 -__-");
+
+console.log("End");
